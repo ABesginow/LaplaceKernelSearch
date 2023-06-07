corrected variance bugs

hardcoded_main.py
@@ -354,9 +354,8 @@ def run_experiment(config):
 
     """
     torch.manual_seed(50)
-    metrics = ["MLL", "MAP", "AIC", "BIC", "MC", "Laplace", "Laplace_prior"]
-    #metrics = ["BIC", "MAP"]
-    #metrics = ["Laplace_prior"]
+    metrics = ["AIC", "BIC", "MC", "Laplace", "Laplace_prior"]
+    metrics = ["Laplace"]
     eval_START = -5
     eval_END = 5
     eval_COUNT = config["num_data"]
@@ -368,11 +367,11 @@ def run_experiment(config):
     data_scaling = True
     use_BFGS = True
     num_draws = 10000
-    parameter_punishment = 2.0
+    parameter_punishment = "BIC"
 
     # set training iterations to the correct config
     options["training"]["max_iter"] = int(train_iterations)
-
+    options["training"]["restarts"] = 2
 
     # training data for model initialization (e.g. 1 point with x=0, y=0) ; this makes initializing the model easier
     prior_x = torch.linspace(0, 1, 1)
@@ -411,8 +410,10 @@ def run_experiment(config):
 
     observations_x = (observations_x - torch.mean(observations_x)
                       ) / torch.std(observations_x)
-    observations_y = (observations_y - torch.mean(observations_y)
-                      ) / torch.std(observations_y)
+    noise_level = 0.4
+    observations_y = observations_y + torch.randn(observations_y.shape) * torch.tensor(noise_level)
+    #observations_y = (observations_y - torch.mean(observations_y)
+    #                  ) / torch.std(observations_y)
 
     f, ax = plt.subplots()
     ax.plot(observations_x, observations_y, 'k*')
@@ -442,7 +443,8 @@ def run_experiment(config):
     for exp_num in range(EXPERIMENT_REPITITIONS):
 
         #model_kernels = ["MAT32+PER"]
-        model_kernels = ["C*C*SE", "SE", "PER", "MAT32", "PER*SE", "PER+SE", "MAT32*PER", "MAT32+PER", "MAT32+SE", "MAT32*SE"]
+        model_kernels = ["C*C*SE", "PER", "PER*SE", "MAT32+PER", "SE", "MAT32", "MAT32+SE", "MAT32*SE"]
+        #model_kernels = ["C*C*SE", "SE", "PER", "MAT32", "MAT32+SE", "MAT32*SE"]#"PER*SE", "PER+SE", "MAT32*PER", "MAT32+PER",
         #model_kernels = ["MAT32*PER"]
 
         for model_kernel in model_kernels:
@@ -475,7 +477,7 @@ def run_experiment(config):
                         continue
                 if loss is np.nan:
                     raise ValueError("training fucked up")
-            train_time = train_end - train_start
+                train_time = train_end - train_start
             if "Laplace" in metrics:
                 laplace_approx, LApp_log = calculate_laplace(
                     model, (-loss)*len(*model.train_inputs), param_punish_term=parameter_punishment)
@@ -538,7 +540,7 @@ def run_experiment(config):
                 BIC_logs["details"] = BIC_log
                 logables["BIC"][model_kernel] = BIC_logs
 
-            if "MAP" in metrics:
+            if "MAP" in metrics or "Laplace_prior" in metrics:
                 loss = np.nan
                 print("\n###############")
                 print(model_kernel)
@@ -563,11 +565,12 @@ def run_experiment(config):
                         continue
                 if loss is np.nan:
                     raise ValueError("training fucked up")
-                MAP_logs = dict()
-                MAP_logs["loss"] = -loss * len(observations_x)
-                MAP_logs["Train time"] = train_end - train_start
-                MAP_logs["model parameters"] = list(model.named_parameters())
-                logables["MAP"][model_kernel] = MAP_logs
+                if "MAP" in metrics:
+                    MAP_logs = dict()
+                    MAP_logs["loss"] = -loss * len(observations_x)
+                    MAP_logs["Train time"] = train_end - train_start
+                    MAP_logs["model parameters"] = list(model.named_parameters())
+                    logables["MAP"][model_kernel] = MAP_logs
 
                 model.eval()
                 likelihood.eval()
@@ -586,7 +589,7 @@ def run_experiment(config):
 
             # Laplace approximation including prior requires different loss
             if "Laplace_prior" in metrics:
-                approx, Lapp_prior_log = calculate_laplace(model, (-loss)*len(*model.train_inputs), with_prior=True)
+                approx, Lapp_prior_log = calculate_laplace(model, (-loss)*len(*model.train_inputs), with_prior=True, param_punish_term = 0.0)
                 Lap_prior_logs = dict()
                 Lap_prior_logs["loss"] = approx
                 Lap_prior_logs["Train time"] = train_end - train_start
@@ -595,10 +598,8 @@ def run_experiment(config):
 
 
 
-
-
             # Perform MCMC
-            if "MC" in metrics and "MLL" in metrics:
+            if "MC" in metrics:
                 MCMC_approx, MC_log = calculate_mc_STAN(
                     model, likelihood, num_draws)
                 MC_logs = dict()
@@ -611,7 +612,7 @@ def run_experiment(config):
     experiment_path = os.path.join("results", "hardcoded",  f"{eval_COUNT}_{data_kernel}")
     if not os.path.exists(experiment_path):
         os.makedirs(experiment_path)
-    with open(os.path.join(experiment_path, f"other_datasizes.pickle"), 'wb') as fh:
+    with open(os.path.join(experiment_path, f"LLapCorBIC.pickle"), 'wb') as fh:
         pickle.dump(logables, fh)
 
     return 0
@@ -620,8 +621,9 @@ def run_experiment(config):
 with open("FINISHED.log", "r") as f:
     finished_configs = [line.strip().split("/")[-1] for line in f.readlines()]
 curdir = os.getcwd()
-num_data = [70, 30, 20]#[200, 150, 100, 50, 10]
-data_kernel = ["SE", "PER", "MAT32", "PER*SE", "PER+SE", "MAT32*PER", "MAT32+PER", "MAT32+SE", "MAT32*SE"]
+num_data =  [10, 20, 50, 70, 100, 150, 200] # TODO: 5, 30, 40 Datapoints    - Done: 10, 20, 50,
+data_kernel = ["SE", "MAT32", "MAT32+SE", "MAT32*SE"]#"PER", "PER*SE", "MAT32+PER", 
+# "PER", "PER*SE", "PER+SE", "MAT32*PER", "MAT32+PER" 
 #data_kernel = ["PER+SE"]
 temp = product(num_data, data_kernel)
 configs = [{"num_data": n, "data_kernel": dat} for n, dat in temp]

main.py
@@ -98,7 +98,7 @@ def run_experiment(config_file, torch_seed):
 
     """
     torch.manual_seed(torch_seed)
-    EXPERIMENT_REPITITIONS = 1
+    EXPERIMENT_REPITITIONS = 5
     total_time_start = time.time()
     options["kernel search"]["print"] = False
     options["training"]["restarts"] = 2 
@@ -131,7 +131,7 @@ def run_experiment(config_file, torch_seed):
 
         log_name = "..."
         experiment_keyword = var_dict["experiment name"]
-        experiment_path = os.path.join("results_100_04", metric, f"{experiment_keyword}")
+        experiment_path = os.path.join("results", metric, f"{experiment_keyword}")
         if not os.path.exists(experiment_path):
             os.makedirs(experiment_path)
         log_experiment_path = os.path.join(experiment_path, f"{experiment_keyword}")
@@ -143,12 +143,13 @@ def run_experiment(config_file, torch_seed):
         # Create base model to generate data
 
         # training data for model initialization (e.g. 1 point with x=0, y=0) ; this makes initializing the model easier
-        prior_x = torch.linspace(0,1,1)
+        prior_x = torch.linspace(0, 1, 1)
         prior_y = prior_x
         # initialize likelihood and model
         data_likelihood = gpytorch.likelihoods.GaussianLikelihood()
         data_model = ExactGPModel(prior_x, prior_y, data_likelihood, kernel_text=data_kernel)
         observations_x = torch.linspace(eval_START, eval_END, eval_COUNT)
+        observations_x = (observations_x - torch.mean(observations_x)) / torch.std(observations_x)
         # Get into evaluation (predictive posterior) mode
         data_model.eval()
         data_likelihood.eval()
@@ -168,7 +169,7 @@ def run_experiment(config_file, torch_seed):
         X = observations_x  #[int((1-train_data_ratio)*0.5*eval_COUNT):int((1+train_data_ratio)*0.5*eval_COUNT)]
         Y = observations_y  #[int((1-train_data_ratio)*0.5*eval_COUNT):int((1+train_data_ratio)*0.5*eval_COUNT)]
 
-        noise_level = 0.4
+        noise_level = 0.1
         # Percentage noise
         Y = Y + torch.randn(Y.shape) * torch.tensor(noise_level)
         # Z-Score scaling
@@ -198,24 +199,49 @@ def run_experiment(config_file, torch_seed):
         # Calculate p_k(y* | X) for the found kernel
         # This is directly comparable for ALL metrics, even MAP-LApp
         
+
+
+        #def manual_log_like(model, likelihood):
+        #    with torch.no_grad():
+        #        observed_pred_prior = likelihood(model(model.train_inputs[0]))
+        #    ## Compare this to the likelihood of y given mean and covar (+ noise)
+        #    #like_mean = torch.zeros(len(model.train_inputs[0]))
+        #    #like_cov_matr = torch.eye(len(model.train_inputs[0].tolist())) * likelihood.noise + model.covar_module(model.train_inputs[0])
+        #    #like_cov_matr += torch.eye(len(model.train_inputs[0].tolist())) * 1e-4 # Jitter
+        #    like_cov_chol = torch.linalg.cholesky(observed_pred_prior.covariance_matrix)
+        #    like_dist = torch.distributions.multivariate_normal.MultivariateNormal(observed_pred_prior.mean, scale_tril=like_cov_chol)
+        #    return like_dist.log_prob(model.train_targets) / model.train_targets.numel()
+
+        #print("post KS")
+        #print(list(model.named_parameters()))
         # Do this without training
         model.eval()
         likelihood.eval()
         test_samples = f_preds.sample_n(10)
         test_samples = test_samples + torch.randn(test_samples.shape) * torch.tensor(noise_level)
         mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
+        #print("post mll init")
+        #print(list(model.named_parameters()))
         test_mll = list()
+        #manual_test_mll = list()
         try:
             for s in test_samples:
                 model.set_train_data(observations_x, s)
+                #print("post new data")
+                #print(list(model.named_parameters()))
                 test_mll.append(mll(model(observations_x), s))
+                #manual_test_mll.append(manual_log_like(model, likelihood))
+                #print("post mll calc")
+                #print(list(model.named_parameters()))
                 #test_mll = [mll(model(observations_x), s) * model.train_targets.numel() for test_sample in test_samples]
         except Exception as E:
             print(E)
             print("----")
             test_mll = [np.nan]
         experiment.store_result("test mll list", test_mll)
+        #experiment.store_result("manual test mll list", manual_test_mll)
         experiment.store_result("avg test mll", torch.mean(torch.Tensor(test_mll)))
+        #experiment.store_result("avg manual test mll", torch.mean(torch.Tensor(manual_test_mll)))
 
 
         try:
@@ -295,7 +321,7 @@ def run_experiment(config_file, torch_seed):
         experiment.write_results(os.path.join(experiment_path, f"{exp_num}.pickle"))
         print(f"END {metric} - {exp_num}/{EXPERIMENT_REPITITIONS} - {time.strftime('%Y-%m-%d %H:%M', time.localtime())}")
         # TODO write filename in FINISHED.log
-    with open("FINISHED.log", "a") as f:
+    with open("FINISHED_var.log", "a") as f:
         f.writelines(config_file + "\n")
 
     m, s = divmod(time.time() - total_time_start, 60)
@@ -312,7 +338,7 @@ if __name__ == "__main__":
     with open("FINISHED.log", "r") as f:
         finished_configs = [line.strip().split("/")[-1] for line in f.readlines()]
     curdir = os.getcwd()
-    keywords = ["MLL", "AIC", "BIC", "Laplace", "Laplace_prior"] # "MC" only when there's a lot of time
+    keywords = ["AIC", "BIC", "MLL",  "Laplace", "Laplace_prior"]#"MLL" sucks  # "MC" only when there's a lot of time
     configs = []
     for KEYWORD in keywords:
         configs.extend([os.path.join(curdir, "configs", KEYWORD, item) for item in os.listdir(os.path.join(curdir, "configs", KEYWORD))])

metrics.py
@@ -110,6 +110,7 @@ def calculate_laplace(model, loss_of_model, variances_list=None, with_prior=Fals
                 'MAT32': {'raw_lengthscale' :{"mean": 1.5711054238673443, "std":2.4453761235991216 } },
                 'RQ': {'raw_lengthscale' :{"mean": -0.049841950913676276, "std":1.9426354614713097 }, 
                         'raw_alpha' :{"mean": 1.882148553921053, "std":3.096431944989054 } },
+                'CosineKernel':{'raw_period_length':{"mean": 0.6485334993738499, "std":0.9930632050553377 }},
                 'PER':{'raw_lengthscale':{"mean": 0.7778461197268618, "std":2.288946656544974 },
                         'raw_period_length':{"mean": 0.6485334993738499, "std":0.9930632050553377 } },
                 'LIN':{'raw_variance' :{"mean": -0.8017903983055685, "std":0.9966569921354465 } },
@@ -161,6 +162,7 @@ def calculate_laplace(model, loss_of_model, variances_list=None, with_prior=Fals
 
     # sigma is a matrix of variance priors
     sigma = torch.diag(torch.Tensor(variances_list))
+    sigma = sigma@sigma
     params = torch.tensor(params_list).clone().reshape(-1, 1)
 
     end = time.time()
@@ -444,6 +446,7 @@ def calculate_mc_STAN(model, likelihood, num_draws):
 
     # sigma is a matrix of variance priors
     sigma = torch.diag(torch.Tensor(variances_list))
+    sigma = sigma@sigma
 
 
     STAN_code = generate_STAN_code(covar_string, debug_param_name_list, covar_string_list)
@@ -511,13 +514,17 @@ def calculate_mc_STAN(model, likelihood, num_draws):
 
 
         try:
+            with torch.no_grad():
+                observed_pred_prior = likelihood(model(model.train_inputs[0]))
             # Compare this to the likelihood of y given mean and covar (+ noise)
-            like_mean = torch.zeros(len(model.train_inputs[0]))
+            #like_mean = torch.zeros(len(model.train_inputs[0]))
 
-            like_cov_matr = torch.eye(len(model.train_inputs[0].tolist())) * likelihood.noise + model.covar_module(model.train_inputs[0])
-            like_cov_matr += torch.eye(len(model.train_inputs[0].tolist())) * 1e-4 # Jitter
-            like_cov_chol = torch.linalg.cholesky(like_cov_matr.evaluate())
-            like_dist = torch.distributions.multivariate_normal.MultivariateNormal(like_mean, scale_tril=like_cov_chol)
+            #like_cov_matr = torch.eye(len(model.train_inputs[0].tolist())) * likelihood.noise + model.covar_module(model.train_inputs[0])
+            #like_cov_matr += torch.eye(len(model.train_inputs[0].tolist())) * 1e-4 # Jitter
+            #like_cov_chol = torch.linalg.cholesky(like_cov_matr.evaluate())
+            #like_dist = torch.distributions.multivariate_normal.MultivariateNormal(like_mean, scale_tril=like_cov_chol)
+            like_cov_chol = torch.linalg.cholesky(observed_pred_prior.covariance_matrix)
+            like_dist = torch.distributions.multivariate_normal.MultivariateNormal(observed_pred_prior.mean, scale_tril=like_cov_chol)
             manual_lp_list.append(like_dist.log_prob(model.train_targets))
         except Exception as e:
             bad_entries += 1