7fd6fe6b0ae21874c96fdcf43f51ac8498052bf6
Andreas Besginow <andreas.besginow@th-owl.de>
2023-10-06 08:41:04 +0200

    Revert "running hardcoded with more settings to see if the trend continues"
    This reverts commit eaee2432b04f87f789d6a1fd826fb79dad6fa90c.

--- hardcoded_main.py
+++ hardcoded_main.py
@@ -395,7 +395,7 @@ def run_experiment(config):
     Returns nothing
 
     """
-    torch.manual_seed(43)
+    torch.manual_seed(45)
     metrics = ["AIC", "BIC", "MC", "Laplace_prior", "MLL", "MAP"]
     eval_START = -5
     eval_END = 5
@@ -700,7 +700,7 @@ def run_experiment(config):
                 exp_num_result_dict["MC"][model_kernel] = MC_logs
         logables["results"].append(exp_num_result_dict)
 
-    experiment_path = os.path.join("results_small_experiment", "hardcoded43",  f"{eval_COUNT}_{data_kernel}")
+    experiment_path = os.path.join("results_small_experiment", "hardcoded",  f"{eval_COUNT}_{data_kernel}")
     if not os.path.exists(experiment_path):
         os.makedirs(experiment_path)
     with open(os.path.join(experiment_path, f"results.pickle"), 'wb') as fh:
@@ -712,8 +712,8 @@ def run_experiment(config):
 with open("FINISHED.log", "r") as f:
     finished_configs = [line.strip().split("/")[-1] for line in f.readlines()]
 curdir = os.getcwd()
-num_data =  [5, 10, 20, 30, 50]#[5, 10, 20, 30, 50, 70, 100, 150, 200]
-data_kernel = ["MAT52", "RQ", "SE", "MAT32", "SE*SE"]
+num_data =  [5, 10, 20]#[5, 10, 20, 30, 50, 70, 100, 150, 200]
+data_kernel = ["MAT52", "RQ", "SE", "MAT32"]
 #data_kernel = ["SE", "RQ", "MAT32", "MAT52", "SE*SE",
 #               "SE+SE", "MAT32+SE", "MAT52+SE", "MAT32*SE", "PER",
 #               "PER*SE", "(SE+RQ)*PER", "SE+SE+SE", "MAT32+(MAT52*PER)"]
